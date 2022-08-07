3f8664dba0ea5c33adcfaf81c1afeda1bbcc2bac
Mainak Mitra <97607911+mainak4ever@users.noreply.github.com>
2022-08-07 14:17:02 +0530

    Data Science Beginner

---
+++ Python/Assign 1/Module.py
@@ -0,0 +1,13 @@
+def addition(num1,num2):
+    return(num1+num2)
+
+
+def substraction(num1,num2):
+    return(num1-num2)
+
+
+def multiplication(num1,num2):
+    return(num1*num2)
+
+def division(num1,num2):
+    return(num1/num2)
